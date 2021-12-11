6406f7b63e93aa3eb1044d48514e379fdd67b2c6
HungryDev <algorithm1401@gmail.com>
2021-12-11 23:14:46 +0100

    Add click listener for registering vertice locations.. Misunderstood the assignment

--- src/main/java/com/godcomplex/ParticleEffectsPlugin.java
+++ src/main/java/com/godcomplex/ParticleEffectsPlugin.java
@@ -1,6 +1,8 @@
 package com.godcomplex;
 
 import com.godcomplex.commands.ParticleCommand;
+import com.godcomplex.listeners.PlayerClickListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ParticleEffectsPlugin extends JavaPlugin {
@@ -8,5 +10,6 @@ public class ParticleEffectsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("shape").setExecutor(new ParticleCommand());
+        Bukkit.getServer().getPluginManager().registerEvents(new PlayerClickListener(), this);
     }
 }
---
+++ src/main/java/com/godcomplex/listeners/PlayerClickListener.java
@@ -0,0 +1,59 @@
+package com.godcomplex.listeners;
+
+import com.godcomplex.domain.ParticleShapeType;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+import java.util.*;
+
+public class PlayerClickListener implements Listener {
+
+    private static final HashMap<UUID, List<Location>> PLAYER_VERTICES_SELECTION_MAP = new HashMap<>();
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent e) {
+        if (!e.getAction().equals(Action.RIGHT_CLICK_AIR) && !e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            return;
+        }
+
+        if (e.getHand() == null || !e.getHand().equals(EquipmentSlot.HAND)) {
+            return;
+        }
+
+        Player clickerPlayer = e.getPlayer();
+        UUID playerUuid = clickerPlayer.getUniqueId();
+        List<Location> VERTICES_LIST = PLAYER_VERTICES_SELECTION_MAP.getOrDefault(playerUuid, new ArrayList<>());
+
+        Location targetLocation = clickerPlayer.getEyeLocation();
+        Block targetBlock = clickerPlayer.getTargetBlock(10);
+        if (targetBlock != null) {
+            targetLocation = targetBlock.getLocation();
+        }
+
+        VERTICES_LIST.add(targetLocation);
+        StringJoiner locationJoiner = new StringJoiner(", ");
+        locationJoiner.add(targetLocation.getX() + "");
+        locationJoiner.add(targetLocation.getY() + "");
+        locationJoiner.add(targetLocation.getZ() + "");
+        clickerPlayer.sendMessage("§aAdded location " + locationJoiner + " to vertices list");
+
+        if (VERTICES_LIST.size() == 1) {
+            PLAYER_VERTICES_SELECTION_MAP.put(playerUuid, VERTICES_LIST);
+            return;
+        }
+
+        if (VERTICES_LIST.size() == 3) {
+            ParticleShapeType.LINE.getShapeDrawer().drawShape(e.getPlayer(), List.of(VERTICES_LIST.get(0), VERTICES_LIST.get(1)));
+            ParticleShapeType.LINE.getShapeDrawer().drawShape(e.getPlayer(), List.of(VERTICES_LIST.get(0), VERTICES_LIST.get(2)));
+            ParticleShapeType.LINE.getShapeDrawer().drawShape(e.getPlayer(), List.of(VERTICES_LIST.get(1), VERTICES_LIST.get(2)));
+            PLAYER_VERTICES_SELECTION_MAP.remove(playerUuid);
+        }
+    }
+
+}
